3dc45569fbc8386978bed51f3fa0ccc6a5c83557
Matheus Zacarias <matheuus412@gmail.com>
2024-02-03 12:25:23 -0300

    🎉 Commit inicial

---
+++ src/test/java/br/com/norrels/courseapi/CoursesApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.norrels.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CoursesApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
